6f1125734fc3f72697ea76f1be27b4271a92f26d
moises <elnaufrago2009@hotmail.com>
2014-04-03 18:56:53 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sheepdeveloper::Application.config.secret_key_base = '46d6e8c781cc8e09be9b694c775a215457ff0eafde5409b1bd1585bcd45563bcd6a4961b2f6c3a467bca4dbafee9ef3c86431f66606fc2e12cc7cb8770704d8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sheepdeveloper::Application.config.session_store :cookie_store, key: '_sheepdeveloper_session'
